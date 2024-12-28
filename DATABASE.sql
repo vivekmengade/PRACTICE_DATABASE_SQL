db5c94a4cd7a9992fb03b9338108210941e298c5
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2024-12-28 20:23:55 +0530

    Create DATABASE.sql
    just practicing MYSQL this is about create database and creating table in it and take view of table.

---
+++ DATABASE.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE PracticeDB;
+create table Students(Sr.no int, Id int primary key, Name varchar(10));
+insert into Student values(1,101,"Vivek");
+select * from Student;
+insert into Student values (2,102,"Nihar");
+select * from Student;
