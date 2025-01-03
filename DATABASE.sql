ade8fc3ecedb2bd874121093b8986da5088cad56
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2025-01-03 17:03:45 +0530

    Update DATABASE.sql
    learned about :- 
    between 
    or
    and
    in
    not in
    like
    not like 
    distinct

--- DATABASE.sql
+++ DATABASE.sql
@@ -46,3 +46,24 @@ select name from student where address = 'pune';
 select * from student;
 select r_no from student;
 select r_no , name from student;
+
+
+
+
+ select * from student where marks between 14 and 18;
+ select name from student where address='mengadewadi' or address='pune' and country='india';
+ select name from student where country='india' and address='mengadewadi' or address='pune';
+ select name from student where country='india' or address='mengadewadi' and address='pune';
+ select name from student where r_no in('1','3','5');
+ select name from student where address in('pune', 'mengadewadi','sangamner');
+ select name from student where address not in('pune', 'mengadewadi','sangamner');
+select r_no from student where marks like'1%';
+ select r_no,name from student where marks like'1%';
+select r_no,name from student where name like'%k';
+select * from student where address like 'm%';
+ select * from student where address like 'p_%';
+select * from student where address like '%__e';
+select * from student where name not like 'a';
+ select distinct * from student;
+ insert into student values(7, 'damini', 'gangamner', '9874561230' , 20, 'Russia');
+ select distinct * from student;
